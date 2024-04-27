aac8c92e1723489296da8398dae45791370e2924
ookapratama <ookapratama2@gmail.com>
2024-04-27 18:02:29 +0800

    tambah splash screen

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,10 +1,23 @@
-import { Text } from "react-native";
-
-
-
+import { Image, StyleSheet, Text, View } from "react-native";
 
 const Splash = () => {
-    return <Text>Halaman Splash</Text>
-}
+  return (
+    <View
+      style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#86BBD8" }}
+    >
+      <Image
+        style={styles.imageSplash}
+        source={require("../../assets/police.png")}
+        resizeMode="contain"
+      />
+    </View>
+  );
+};
 
 export default Splash;
+
+const styles = StyleSheet.create({
+  imageSplash: {
+    width: 350,
+  },
+});
